a029fd3de926a747593cb1ea7851fc4933ba7d1d
bakseungho <bakseungho@naver.com>
2024-05-17 23:49:39 +0900

    Design:헤더&사이드바 UI 구현 #2

--- teambeam/app/teamPage/_components/SideBar.tsx
+++ teambeam/app/teamPage/_components/SideBar.tsx
@@ -2,37 +2,75 @@
 
 import Link from "next/link";
 import "./SideBar.scss";
+import {
+  BoardIcon,
+  CalendarIcon,
+  CommentIcon,
+  HomeIcon,
+  SettingIcon,
+  TodoIcon,
+} from "@/app/_components/Icons";
 
 export default function SideBar() {
   return (
     <nav>
-      <div className='top_info'>
-        <h3>팀 프로젝트</h3>
+      <div className='top-info'>
+        <h2>팀 프로젝트</h2>
       </div>
 
-      <div className='side_menu'>
+      <div className='side-menu'>
         <li>
-          <Link href='/teamPage/teamMain'>메인</Link>
+          <Link href='/teamPage/teamMain'>
+            <div className='icon-box'>
+              <HomeIcon size={21} />
+            </div>
+            <span>메인</span>
+          </Link>
         </li>
 
         <li>
-          <Link href='/teamPage/teamCalendar'>캘린더</Link>
+          <Link href='/teamPage/teamCalendar'>
+            <div className='icon-box'>
+              <CalendarIcon size={16} />
+            </div>
+            <span>캘린더</span>
+          </Link>
         </li>
 
         <li>
-          <Link href='/teamPage/teamTodo'>투드리스트</Link>
+          <Link href='/teamPage/teamTodo'>
+            <div className='icon-box'>
+              <TodoIcon size={18} />
+            </div>
+            <span>투드리스트</span>
+          </Link>
         </li>
 
         <li>
-          <Link href='/teamPage/teamBoard'>게시판</Link>
+          <Link href='/teamPage/teamBoard'>
+            <div className='icon-box'>
+              <BoardIcon size={14} />
+            </div>
+            <span>게시판</span>
+          </Link>
         </li>
 
         <li>
-          <Link href='/teamPage/teamChatting'>채팅</Link>
+          <Link href='/teamPage/teamChatting'>
+            <div className='icon-box'>
+              <CommentIcon size={18} />
+            </div>
+            <span>채팅</span>
+          </Link>
         </li>
 
         <li>
-          <Link href='/teamPage/teamSetting'>프로젝트 관리</Link>
+          <Link href='/teamPage/teamSetting'>
+            <div className='icon-box'>
+              <SettingIcon size={18} />
+            </div>
+            <span>프로젝트 관리</span>
+          </Link>
         </li>
       </div>
     </nav>
