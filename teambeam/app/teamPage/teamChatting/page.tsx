7d4f5c5f7380c242dd6feecc7b8e67796745b195
Hayoung Shim <gkdud3579@gmail.com>
2024-05-17 10:58:44 +0900

    Design: 헤더&사이드바 적용으로 인한 채팅 UI 레이아웃 수정 #2

--- teambeam/app/teamPage/teamChatting/page.tsx
+++ teambeam/app/teamPage/teamChatting/page.tsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import Header from '../../../components/header/Header';
-import Sidebar from '../../../components/sidebar/Sidebar';
 import ChatList from './components/ChatList';
 import './styles/main.scss';
 
 const TeamChatting: React.FC = () => {
   return (
     <div className="container">
-      <Header />
-      <div className="main-content">
-        <Sidebar />
         <div className="chat-area">
           <ChatList />
         </div>
       </div>
-    </div>
   );
 };
 
