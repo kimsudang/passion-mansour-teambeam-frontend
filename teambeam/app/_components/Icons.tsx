dad8970314566816f49f966d3cbcecfd07c98a59
Hayoung Shim <gkdud3579@gmail.com>
2024-05-20 16:41:34 +0900

    팀투두리스트 페이지에서 사용할 아이콘 추가#1

--- teambeam/app/_components/Icons.tsx
+++ teambeam/app/_components/Icons.tsx
@@ -180,3 +180,12 @@ export const HamburgerIcon = ({ size }: { size: number }) => {
     </svg>
   );
 };
+
+// EllipsisBtn
+export const EllipsisBtn = ({ size }: { size: number }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} viewBox="0 0 128 512">
+      <path d="M64 360a56 56 0 1 0 0 112 56 56 0 1 0 0-112zm0-160a56 56 0 1 0 0 112 56 56 0 1 0 0-112zM120 96A56 56 0 1 0 8 96a56 56 0 1 0 112 0z" />
+    </svg>
+  );
+};
