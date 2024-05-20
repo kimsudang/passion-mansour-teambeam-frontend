e6f93999b94e71fbe89bc4461e52556dd98819d2
Hayoung Shim <gkdud3579@gmail.com>
2024-05-20 16:33:54 +0900

    팀투두리스트 페이지에서 사용할 아이콘 추가#1

--- teambeam/app/_components/Icons.tsx
+++ teambeam/app/_components/Icons.tsx
@@ -157,7 +157,7 @@ export const LeftBtnIcon = ({ size }: { size: number }) => {
 // ToggleBtn
 export const ToggleBtnIcon = ({ size }: { size: number }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} viewBox="0 0 512 512">
       <path d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z" />
     </svg>
   );
@@ -166,7 +166,7 @@ export const ToggleBtnIcon = ({ size }: { size: number }) => {
 // PlusBtn
 export const PlusBtnIcon = ({ size }: { size: number }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} viewBox="0 0 448 512">
       <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
     </svg>
   );
@@ -175,7 +175,7 @@ export const PlusBtnIcon = ({ size }: { size: number }) => {
 // HamburgerBtn
 export const HamburgerIcon = ({ size }: { size: number }) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+    <svg xmlns="http://www.w3.org/2000/svg" width={size} viewBox="0 0 448 512">
       <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
     </svg>
   );
