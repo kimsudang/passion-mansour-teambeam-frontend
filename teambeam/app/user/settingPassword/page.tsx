a4a993b039bc01ca2ef2aa9d3cb65f3803cb4a9a
kim suyeon <0ksuyeon0@gmail.com>
2024-05-26 23:27:40 +0900

    Feat: 비밀번호 변경 기능
    비밀번호 찾기에서 접근하는 비밀번호 변경 기능 구현. api 통신 코드는 구현하지 않았습니다.

--- teambeam/app/user/settingPassword/page.tsx
+++ teambeam/app/user/settingPassword/page.tsx
@@ -1,18 +1,64 @@
-import React from "react";
+'use client'
+
+import React, { useState } from "react";
 import './layout.scss';
 
 const SettingPassword = () => {
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const validatePassword = (password: string) => {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!validatePassword(newPassword)) {
+      setMessage("비밀번호는 영문, 숫자, 기호로 구성된 8자리 이상이어야 합니다.");
+      return;
+    }
+    if (newPassword !== confirmPassword) {
+      setMessage("비밀번호가 일치하지 않습니다.");
+      return;
+    }
+
+    // API 호출 대신에 비밀번호 변경 성공 메시지 출력
+    setMessage("비밀번호가 성공적으로 변경되었습니다.");
+  };
+
   return (
     <div className="setting_password_container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="setting_box">
           <p className="page_title">비밀번호 변경</p>
-          <input type="text" name="password" placeholder="비밀번호를 입력해주세요." />
-          <p className="warning_msg">오류 로그</p>
-          <input type="text" name="check_password" placeholder="비밀번호를 다시 입력해주세요." />
-          <p className="warning_msg">오류 로그</p>
-        <button className="change_password">비밀번호 변경하기</button>
-      </div>
+          <div>
+            <label htmlFor="newPassword"></label>
+            <input
+              type="password"
+              id="newPassword"
+              placeholder="비밀번호를 입력해주세요."
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="confirmPassword"></label>
+            <input
+              type="password"
+              id="confirmPassword"
+              placeholder="비밀번호를 다시 입력해주세요."
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+          </div>
+          {message && <p>{message}</p>}
+          <button className="change_password" type="submit">
+            비밀번호 변경하기
+          </button>
+        </div>
       </form>
     </div>
   );
