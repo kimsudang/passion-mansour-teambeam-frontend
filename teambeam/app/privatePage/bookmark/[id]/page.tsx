49e0b84ea4caa6f0083f034e3137fa0e6e86620d
bakseungho <bakseungho@naver.com>
2024-05-28 04:16:07 +0900

    Feat:표 기능 구현 #6

--- teambeam/app/privatePage/bookmark/[id]/page.tsx
+++ teambeam/app/privatePage/bookmark/[id]/page.tsx
@@ -3,12 +3,20 @@
 import BoardView from "@/app/_components/BoardView";
 import React, { useState } from "react";
 
+type ContentType = {
+  key: string;
+  value: string;
+};
+
 export type BoardType = {
   postId: number;
   postTitle: string;
   postType: string;
-  postContent: string;
-  writer: string;
+  postContent: ContentType[][] | string;
+  writer: {
+    memberId: number;
+    memberName: string;
+  };
   createDate: string;
   updateDate: string;
   tags: { tagId: number; tagName: string }[];
@@ -29,10 +37,28 @@ const Page = () => {
   const [boardData, setBoardData] = useState<BoardType>({
     postId: 0,
     postTitle: "게시글 제목",
-    postType: "board",
-    postContent:
-      "게시글 내용입니다 게시글 내용입니다 게시글 내용입니다 게시글 내용입니다 게시글 내용입니다 게시글 내용입니다 게시글 내용입니다 게시글 내용입니다 게시글 내용입니다 게시글 내용입니다  게시글 내용입니다",
-    writer: "홍길동",
+    postType: "table",
+    postContent: [
+      [
+        { key: "0_0", value: "HTTP" },
+        { key: "0_1", value: "기능" },
+        { key: "0_2", value: "URL" },
+      ],
+      [
+        { key: "1_0", value: "GET" },
+        { key: "1_1", value: "로그인" },
+        { key: "1_2", value: "/api/user/login" },
+      ],
+      [
+        { key: "2_0", value: "POST" },
+        { key: "2_1", value: "프로젝트 생성" },
+        { key: "2_2", value: "/api/project" },
+      ],
+    ],
+    writer: {
+      memberId: 0,
+      memberName: "홍길동",
+    },
     createDate: "2024-04-23 09:51:13",
     updateDate: "2024-04-23 09:51:13",
     tags: [
