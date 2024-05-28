da0301656d824178cbf91b9a9c5aa3086d8001bd
bakseungho <bakseungho@naver.com>
2024-05-28 18:25:43 +0900

    Feat:프로젝트 API 생성 및 조회 기능 구현 #17

---
+++ teambeam/app/_api/project.ts
@@ -0,0 +1,57 @@
+import api from "@/app/_api/api";
+import { AxiosError } from "axios";
+
+// 프로젝트 리스트 조회
+export const getPorjectList = async (url: string) => {
+  try {
+    const res = await api.get(url, {
+      headers: {
+        Authorization: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+      },
+      withCredentials: true,
+    });
+
+    return res;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.error(
+        "Error fetching calendar events:",
+        error.response?.data || error.message
+      );
+    } else {
+      console.error("Error fetching calendar events:", error);
+    }
+    throw error;
+  }
+};
+
+// 프로젝트 생성
+export const postPorject = async (url: string, data: any) => {
+  try {
+    const res = await api.post(
+      url,
+      {
+        projectName: data.title,
+        description: data.content,
+      },
+      {
+        headers: {
+          Authorization: process.env.NEXT_PUBLIC_ACCESS_TOKEN,
+        },
+        withCredentials: true,
+      }
+    );
+
+    return res;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      console.error(
+        "Error fetching calendar events:",
+        error.response?.data || error.message
+      );
+    } else {
+      console.error("Error fetching calendar events:", error);
+    }
+    throw error;
+  }
+};
--- teambeam/app/_components/AddModal.tsx
+++ teambeam/app/_components/AddModal.tsx
@@ -3,13 +3,12 @@
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 import "@/app/_styles/AddModal.scss";
+import { postPorject } from "../_api/project";
 
 type FormType = {
   title: string;
   content: string;
 };
-
-const token = `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhIiwiaWF0IjoxNzE2Mzg1MDc5LCJleHAiOjE3MTYzODU2Nzl9.qy-6-3HczlDCoN-jyWHI2Mzj7MpyFYfYqQHYAv8nYY8J2b3hAvOsHoplYzQss4S3torLahL4rMRaYcGuzHhI8A`;
 
 export default function AddModal({
   onCloseModal,
@@ -44,29 +43,17 @@ export default function AddModal({
 
     console.log("title : ", form.title);
     console.log("content : ", form.content);
-    console.log("token", token);
 
-    /*
-    axios
-      .post(
-        "http://34.22.108.250:8080/api/project",
-        {
-          projectName: form.title,
-          description: form.content,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-            RefreshToken: `eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhIiwiaWF0IjoxNzE2Mzg1MDc5LCJleHAiOjE3MTY5ODk4Nzl9.oAowrexnXs5b1MIO-UlIT-PRKL7fotsUmc0eSB4j8kcsgK68bhJnQzf1hm_88jdCPfO34xSz2WxGxqIienJJ6w`,
-          },
-        }
-      )
-      .then((res) => {
-        if (res.status === 201) router.push("/teamPage/main");
-      })
-      .catch((err) => console.log(err));
-      */
-  }, [form]);
+    try {
+      const res = await postPorject("/project", form);
+      console.log("res : ", res);
+
+      alert("프로젝트 생성이 완료되었습니다.");
+      router.push("/teamPage/teamMain");
+    } catch (err) {
+      console.log("err  : ", err);
+    }
+  }, [form, router]);
 
   return (
     <div className='modal-bg'>
