9d0f7f328416ce3a294d9aed58d593e1eb86ff0b
kim suyeon <0ksuyeon0@gmail.com>
2024-05-29 15:58:24 +0900

    feat: 이메일 로그인 기능 API 연결 #10
    이메일로 로그인 하는 부분 API 연결 완료했습니다!

--- teambeam/app/user/login/page.tsx
+++ teambeam/app/user/login/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Link from "next/link";
+import { useRouter } from 'next/navigation';
+import axios from 'axios';
 import "./layout.scss";
 import useForm from "../../_hooks/useForm";
+import ForgotPasswordModal from "./_components/ForgotPasswordModal";
 
 interface ILoginForm {
   mail: string;
@@ -11,16 +14,43 @@ interface ILoginForm {
 }
 
 const Login: React.FC = () => {
+  const router = useRouter();
+  const CLIENT_ID = process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID;
+  const REDIRECT_URI = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI;
+
   const { values, errors, isLoading, handleChange, handleSubmit } =
     useForm<ILoginForm>({
       initialValues: { mail: "", password: "" },
-      onSubmit: (data) => {
-        // 더미 데이터로 테스트하기 위해 콘솔에 출력합니다.
-        console.log("폼 데이터:", data);
+      onSubmit: async (data) => {
+        try {
+          const response = await axios.post("http://34.22.108.250:8080/api/login", data);
 
-        // 이메일과 비밀번호만을 추출하여 콘솔에 출력합니다.
-        // const { mail, password } = data;
-        // console.log("추출한 데이터:", { mail, password });
+          // 헤더에서 토큰 추출
+          const authorizationToken = response?.headers['authorization'];
+          const refreshToken = response?.headers['refreshtoken'];
+          console.log("authorizationToken : ", authorizationToken);
+          console.log("refreshToken : ", refreshToken);
+
+          // 본문에서 데이터 추출
+          const { message, memberId } = response.data;
+
+          // 토큰 및 사용자 정보 저장
+          if (authorizationToken && refreshToken) {
+            localStorage.setItem("Authorization", authorizationToken);
+            localStorage.setItem("RefreshToken", refreshToken);
+            localStorage.setItem("MemberId", memberId);
+
+            console.log(message);  // "로그인 성공" 메시지 출력
+            alert("로그인 성공");
+            // 로그인 성공 시 대시보드 페이지로 이동
+            router.push('/main');
+          } else {
+            console.error("로그인에 필요한 토큰이 누락되었습니다.");
+          }
+        } catch (error) {
+          console.error("로그인 요청 실패:", error);
+          alert("이메일 혹은 비밀번호가 일치하지 않습니다.");
+        }
       },
       validate: (values) => {
         const errors: Partial<Record<keyof ILoginForm, string>> = {};
@@ -35,7 +65,20 @@ const Login: React.FC = () => {
         }
         return errors;
       },
-    });
+    }
+  );
+
+  // 비밀번호 변경 모달 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = (e: React.MouseEvent) => {
+    e.preventDefault();
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className='login_container'>
@@ -56,7 +99,7 @@ const Login: React.FC = () => {
               <br />
               <input
                 className='submit_password'
-                type='text'
+                type='password'
                 name='password'
                 value={values.password}
                 onChange={handleChange}
@@ -78,7 +121,7 @@ const Login: React.FC = () => {
           <Link className='page_link' href='/user/join'>
             회원가입
           </Link>
-          <Link className='page_link' href='/user/settingPassword'>
+          <Link className='page_link' href="#" onClick={openModal}>
             비밀번호 찾기
           </Link>
         </div>
@@ -87,6 +130,7 @@ const Login: React.FC = () => {
           <button className='login_with_kakao'>카카오로 로그인하기</button>
         </div>
       </div>
+      {isModalOpen && <ForgotPasswordModal onClose={closeModal} />}
     </div>
   );
 };
