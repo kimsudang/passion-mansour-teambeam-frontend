effad70e77303c771e9c43e9c283e2215422e6fd
kimsudang <0ksuyeon0@gmail.com>
2024-06-09 14:19:51 +0000

    Fix: 로그인 유무로 접근 가능 페이지 설정
    로그인이 되어 있다면 시작하기 버튼을, 아니라면 로그인/회원가입 버튼을 보여주도록 수정

--- teambeam/app/teamBeam/page.tsx
+++ teambeam/app/teamBeam/page.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+"use client";
+
+import React, {useEffect, useState} from "react";
 import Link from 'next/link';
 import Header from "../_components/Header";
 import './layout.scss';
 
 const TeambeamHome = () => {
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(()=>{
+      const storedToken = localStorage.getItem("Authorization");
+      setToken(storedToken || '');
+  }, []);
+
   return (
     <>
       <Header />
@@ -14,12 +23,21 @@ const TeambeamHome = () => {
           <p>팀플 <span>일정관리</span>가 고민인 당신</p>
         </div>
         <div className="start_button">
-          <Link href="/user/join" passHref>
-            <button className="join_button">회원가입</button>
-          </Link>
-          <Link href="/user/login" passHref>
-            <button className="login_button">로그인</button>
-          </Link>
+          {!token ? (
+            <>
+              <Link href="/user/join" passHref>
+                <button className="join_button">회원가입</button>
+              </Link>
+              <Link href="/user/login" passHref>
+                <button className="login_button">로그인</button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <Link href="/main" passHref>
+                <button className="main_button">시작하기</button>
+              </Link>
+            </>)}
         </div>
       </div>
     </>
