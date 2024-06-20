072169435b6f0c58811d2cb006ea513ad483c738
kimsudang <0ksuyeon0@gmail.com>
2024-06-20 10:37:46 +0000

    Fix: 인덱스 화면 UI 수정

--- teambeam/app/page.tsx
+++ teambeam/app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-// import styles from "./page.module.css";
 import "./teamBeam/layout.scss";
 import TeambeamHome from "./teamBeam/page";
 
--- teambeam/app/teamBeam/page.tsx
+++ teambeam/app/teamBeam/page.tsx
@@ -1,43 +1,48 @@
 "use client";
 
-import React, {useEffect, useState} from "react";
-import Link from 'next/link';
-import Header from "../_components/Header";
-import './layout.scss';
+import React, { useEffect, useState } from "react";
+import mainImg from "../../public/img/intro_main.png";
+import Link from "next/link";
+import "./layout.scss";
+import Image from "next/image";
 
 const TeambeamHome = () => {
   const [token, setToken] = useState<string | null>(null);
 
-  useEffect(()=>{
-      const storedToken = localStorage.getItem("Authorization");
-      setToken(storedToken || '');
+  useEffect(() => {
+    const storedToken = localStorage.getItem("Authorization");
+    setToken(storedToken || "");
   }, []);
 
   return (
     <>
-      <Header />
-      <div className="teamBeam_container">
-        <div className="comment_box">
-          <p>팀플 <span>일정관리</span>가 고민인 당신</p>
-          <p>팀플 <span>일정관리</span>가 고민인 당신</p>
-          <p>팀플 <span>일정관리</span>가 고민인 당신</p>
+      <div className='teamBeam_container'>
+        <p className='teamBeamTop'>
+          개발 프로젝트
+          <br />
+          통합 일정관리 서비스
+        </p>
+        <div className='comment_box'>
+          <Image src={mainImg} width={440} alt='dfsdf' />
         </div>
-        <div className="start_button">
+        <span className='teamBeamBottom'>&quot;팀 글 벙 글&quot;</span>
+        <div className='start_button'>
           {!token ? (
             <>
-              <Link href="/user/join" passHref>
-                <button className="join_button">회원가입</button>
+              <Link href='/user/join' passHref>
+                <button className='join_button'>회원가입</button>
               </Link>
-              <Link href="/user/login" passHref>
-                <button className="login_button">로그인</button>
+              <Link href='/user/login' passHref>
+                <button className='login_button'>로그인</button>
               </Link>
             </>
           ) : (
             <>
-              <Link href="/main" passHref>
-                <button className="main_button">시작하기</button>
+              <Link href='/main' passHref>
+                <button className='main_button'>시작하기</button>
               </Link>
-            </>)}
+            </>
+          )}
         </div>
       </div>
     </>
